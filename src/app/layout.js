f2f782e804ef13afc34879139100faf4452f9e63
GOLI VASU <golivasu322@gmail.com>
2026-06-18 22:26:57 +0530

    gtm add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from "next/script";
 
 export const metadata = {
   title: 'AI & Machine Learning Career Program for Students | Dandes Academy',
@@ -24,12 +25,44 @@ export const metadata = {
   ],
 };
 
+
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-white text-gray-900">
-        {children}
+      <head>
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W865WG7N');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
+      </head>
+      <body className={`${inter.className} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W865WG7N"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        <main className="pt-[90px] min-h-screen">
+          {children}
+        </main>
+
       </body>
     </html>
-  );
+  )
 }
